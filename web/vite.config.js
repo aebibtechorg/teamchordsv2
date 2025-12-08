0e3a5416faeefe6cc5b33f5e4d8de8426ba2c806
Paul Abib Camano <paul.camano@gmail.com>
2025-12-08 17:25:26 +0800

    tailwind upgrade

--- web/vite.config.js
+++ web/vite.config.js
@@ -1,11 +1,12 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
   const target = env['services__api__http__0'];
   return {
-    plugins: [react()],
+    plugins: [react(), tailwindcss()],
     server:{
       port: parseInt(env.VITE_PORT),
       proxy: {
