5ce3a0871877071cf5165967b9338fd15f0c6a76
paul <paul@aebibtech.com>
2026-04-16 16:55:01 +0800

    Initialize shadcn/ui on the frontend

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,12 +1,19 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
+
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
   const target = env['services__api__http__0'];
   return {
     plugins: [react(), tailwindcss()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src')
+      }
+    },
     server:{
       port: parseInt(env.VITE_PORT),
       proxy: {
@@ -29,7 +36,9 @@ export default defineConfig(({ mode }) => {
     build:{
       outDir: 'dist',
       rollupOptions: {
-        input: './index.html'
+        input: {
+          main: path.resolve(__dirname, 'index.html')
+        }
       }
     }
   }
