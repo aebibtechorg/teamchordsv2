e9076977c4b5036df130202eae70afb061aa40f2
paul <paul@aebibtech.com>
2026-05-01 22:43:22 +0800

    Disable issuer validation in claims configuration

--- tcv2.Api/Program.cs
+++ tcv2.Api/Program.cs
@@ -86,6 +86,7 @@ builder.Services.AddAuthentication(options =>
     {
         NameClaimType = System.Security.Claims.ClaimTypes.NameIdentifier,
         RoleClaimType = "https://teamchordsapp.io/roles",
+        ValidateIssuer = false
     };
 });
 
