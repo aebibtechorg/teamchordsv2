211388468a460e8760ac15f8ccf46d6f70bf0bcc
paul <paul@aebibtech.com>
2026-05-11 23:56:54 +0800

    feat: switch to Vercel adapter and update dependencies
    - Updated astro.config.mjs to use @astrojs/vercel instead of @astrojs/node.
    - Added @astrojs/vercel to package.json and package-lock.json dependencies.

--- blog/astro.config.mjs
+++ blog/astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-import node from '@astrojs/node';
+// import node from '@astrojs/node';
 import sitemap from '@astrojs/sitemap';
+import vercel from '@astrojs/vercel/serverless';
 
 export default defineConfig({
   site: process.env.PUBLIC_SITE_URL || 'https://teamchords.com',
   output: 'server',
   server: { host: true, port: 8080 },
-  adapter: node({ mode: 'standalone' }),
+  adapter: vercel(),
   integrations: [mdx(), sitemap()],
 });
